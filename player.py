f5b060705062ed12bf63fbe8d8a0fb469c203f4a
Petr Velička <petrvelicka@tuta.io>
2022-05-07 20:42:19 +0200

    formatting

--- player.py
+++ player.py
@@ -1,18 +1,19 @@
 import pygame
 
+
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacleSprites):
         super().__init__(groups)
         self.image = pygame.image.load("graphics/player.png").convert_alpha()
-        self.rect = self.image.get_rect(topleft = pos)
+        self.rect = self.image.get_rect(topleft=pos)
 
         self.direction = pygame.math.Vector2()
         self.speed = 5
 
         self.obstacleSprites = obstacleSprites
+
     def input(self):
         keys = pygame.key.get_pressed()
-
 
         if keys[pygame.K_d]:
             self.direction.x = 1
@@ -29,13 +30,12 @@ class Player(pygame.sprite.Sprite):
 
     def collisions(self, direction):
         if direction == 'horizontal':
-            for sprite  in self.obstacleSprites:
+            for sprite in self.obstacleSprites:
                 if sprite.rect.colliderect(self.rect):
                     if self.direction.x > 0:
                         self.rect.right = sprite.rect.left
                     if self.direction.x < 0:
                         self.rect.left = sprite.rect.right
-
 
     def update(self):
         self.input()
