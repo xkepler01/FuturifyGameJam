a70aa4b819692f52e041f079edc67f3677fa536d
Petr Velička <petrvelicka@tuta.io>
2022-05-07 17:12:24 +0200

    Initial Commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+import pygame
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((1280, 720))
+        pygame.display.set_caption("FuturifyGameJam")
+        self.clock = pygame.time.Clock()
+
+    def run(self):
+        while True:
+            # process the events
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # update the gamestate
+            self.screen.fill('white')
+            pygame.display.update()
+            self.clock.tick(60)
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.run()
