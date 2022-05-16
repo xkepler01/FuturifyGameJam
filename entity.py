7efdb77af4e438ba30e05e02ea9758d76bd03e2a
Salaaat <camorka.zelena@seznam.cz>
2022-05-16 19:11:28 +0200

    dva typy krabic xd (tech neni nikdy dost)

--- entity.py
+++ entity.py
@@ -1,5 +1,6 @@
 import pygame
 from map import *
+from random import randint
 
 
 def round_to_multiply(x, base):
@@ -9,7 +10,8 @@ def round_to_multiply(x, base):
 class Box(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacleSprites, playerSprites, berrySprites, finishSprites, entitySprites):
         super().__init__(groups)
-        self.image = pygame.transform.scale(pygame.image.load("graphics/box.png").convert_alpha(), (TILESIZE, TILESIZE))
+        box = ("graphics/box.png", "graphics/box2.png")
+        self.image = pygame.transform.scale(pygame.image.load(box[randint(0,1)]).convert_alpha(), (TILESIZE, TILESIZE))
         self.rect = self.image.get_rect(topleft=pos)
 
         self.direction = pygame.math.Vector2()
